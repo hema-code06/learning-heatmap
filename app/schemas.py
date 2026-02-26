59b6700452aeb5a1bbe8787fe04ca374d456ffa6
hema-code06 <hemavathi.siva06@gmail.com>
2026-02-26 20:18:01 +0530

    added user and learning entry req/res schemas

---
+++ app/schemas.py
@@ -0,0 +1,28 @@
+from pydantic import BaseModel, EmailStr
+from datetime import date
+from uuid import UUID
+
+class UserCreate(BaseModel):
+    email:EmailStr
+    password:str
+    
+class UserLogin(BaseModel):
+    email:EmailStr
+    password:str
+    
+class LearningEntryCreate(BaseModel):
+    date:date
+    hours:float
+    topic:str
+    notes:str | None = None
+    
+class LearningEntryResponse(BaseModel):
+    id:UUID
+    date:date
+    hours:float
+    topic:str
+    notes:str | None
+    
+    class Config:
+        orm_mode = True
+    
