05c6d58315973daf14f2f8c3034d11978c59ea8c
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-07 13:49:16 +0530

    converted productivity card to radial chart

--- frontend/src/components/ProductivityCard.jsx
+++ frontend/src/components/ProductivityCard.jsx
@@ -1,11 +1,42 @@
 import Card from "./ui/Card";
+import { RadialBarChart, RadialBar, ResponsiveContainer } from "recharts";
 
 export default function ProductivityCard({ data }) {
+  if (!data) return null;
+
+  const chartData = [
+    {
+      name: "score",
+      value: data.productivity_score,
+      fill: "#6366F1",
+    },
+  ];
+
   return (
-    <Card>
-      <h2>Productivity Score</h2>
-      <h1>{data.productivity_score}</h1>
-      <p>{data.label}</p>
+    <Card title="Average Performance">
+      <div className="flex flex-col items-center justify-center">
+        <div className="w-full h-64">
+          <ResponsiveContainer>
+            <RadialBarChart
+              innerRadius="70%"
+              outerRadius="100%"
+              data={chartData}
+              startAngle={90}
+              endAngle={-270}
+            >
+              <RadialBar minAngle={15} background clockWise dataKey="value" />
+            </RadialBarChart>
+          </ResponsiveContainer>
+        </div>
+
+        <div className="text-center -mt-20">
+          <h2 className="text-4xl font-bold text-indigo-600">
+            {data.productivity_score}
+          </h2>
+
+          <p className="text-slate-500 text-sm mt-1">{data.label}</p>
+        </div>
+      </div>
     </Card>
   );
 }
