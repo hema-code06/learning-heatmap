91a989c9eab82fb2310f17307cea3c60d8f8f8f6
hema-code06 <hemavathi.siva06@gmail.com>
2026-03-07 17:14:21 +0530

    added pydantic schemas for extra features

--- app/models.py
+++ app/models.py
@@ -1,24 +1,78 @@
 import uuid
 from datetime import datetime
-from sqlalchemy import Column, Integer, String, Date, Float, DateTime, Index, UniqueConstraint
+from sqlalchemy import Column, Integer, String, Date, Float, Text, ForeignKey, DateTime, Boolean, Index, UniqueConstraint
 from sqlalchemy.sql import func
+from sqlalchemy import relationship
+from sqlalchemy import UniqueConstraint
 from sqlalchemy.dialects.postgresql import UUID as PGUUID
 
 
 from .database import Base
 
 
+class Topic(Base):
+    __tablename__ = "topics"
+
+    id = Column(PGUUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    name = Column(String, nullable=False)
+
+    start_date = Column(Date, nullable=True)
+    end_date = Column(Date, nullable=True)
+
+    estimated_hours = Column(Float, nullable=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    subtopics = relationship(
+        "SubTopic", back_populates="topic", cascade="all, delete")
+    projects = relationship(
+        "Project", back_populates="topic", cascade="all, delete")
+
+
+class SubTopic(Base):
+    __tablename__ = "subtopics"
+
+    id = Column(PGUUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+    topic_id = Column(
+        PGUUID(as_uuid=True),
+        ForeignKey("topics.id", ondelete="CASCADE"),
+        nullable=False
+    )
+    name = Column(String, nullable=False)
+    completed = Column(Boolean, default=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    topic = relationship("Topic", back_populates="subtopics")
+
+
+class Project(Base):
+    __tablename__ = "projects"
+
+    id = Column(PGUUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
+
+    topic_id = Column(
+        PGUUID(as_uuid=True),
+        ForeignKey("topics.id", ondelete="CASCADE"),
+        nullable=False
+    )
+
+    name = Column(String, nullable=False)
+
+    description = Column(Text, nullable=True)
+
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    topic = relationship("Topic", back_populates="projects")
+
+
 class LearningEntry(Base):
     __tablename__ = "learning_entries"
 
     __table_args__ = (
-        Index("idx_user_date", "user_id", "date"),
+        Index("idx_date", "date"),
     )
 
     id = Column(PGUUID(as_uuid=True), primary_key=True, default=uuid.uuid4)
-
-    user_id = Column(Integer, default=1)
-
     date = Column(Date, nullable=False)
     hours = Column(Float, nullable=False)
     topic = Column(String, nullable=False)
@@ -40,11 +94,10 @@ class MonthlyGoal(Base):
 class Badge(Base):
     __tablename__ = "badges"
     id = Column(Integer, primary_key=True, index=True)
-    user_id = Column(Integer, default=1)
-
     badge_name = Column(String, nullable=False)
-    unlocked_at = Column(DateTime(timezone=True), server_default=func.now())
+    topic = Column(String, nullable=False)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
 
     __table_args__ = (
-        UniqueConstraint("user_id", "badge_name", name="unique_user_badge"),
+        UniqueConstraint("badge_name", "topic", name="unique_topic_badge"),
     )
